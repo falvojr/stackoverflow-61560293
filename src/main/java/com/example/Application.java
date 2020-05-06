304a47abe7b8a4a2a4be4e00da342e9bffe8727d
Venilton FalvoJr <falvojr@gmail.com>
2020-05-06 15:58:24 -0300

    Project to simulate StackOverflow Question 61560293

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.domain.Log;
+import com.example.domain.LogRepository;
+import com.example.domain.MyClass1;
+import com.example.domain.MyClass2;
+import com.mongodb.MongoClientOptions;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private LogRepository logRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MongoClientOptions mongoClientOption() {
+        // I am using the PojoCodec to automatic parse my domain classes:
+        final CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+        final CodecRegistry fullCodecRegistry = fromRegistries(getDefaultCodecRegistry(), pojoCodecRegistry);
+        return MongoClientOptions.builder().codecRegistry(fullCodecRegistry).build();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        final String key = "response";
+
+        // Simulate my dynamic of entity insertion (with several domain classes into "outbound" field):
+        final Map<String, Object> map = new HashMap<>();
+        map.put(key, new MyClass1("attr1", new MyClass2(1L)));
+        final Log log = new Log();
+        log.setOutbound(new Document(map));
+        logRepository.save(log);
+
+        // Find saved entity to try specific class conversion (MyClass1):
+        final Log logSaved = logRepository.findById(log.getId()).orElseThrow(RuntimeException::new);
+
+        try {
+            // Error occurs here, but in Spring Boot 1.X this conversion/cast works...
+            final MyClass1 test = logSaved.getOutbound().get(key, MyClass1.class);
+            System.out.println(test);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
